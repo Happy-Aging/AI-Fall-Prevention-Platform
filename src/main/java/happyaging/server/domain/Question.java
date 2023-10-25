f8bdeb0e37533abcf7eeb8fdb3df8b44f13f40d6
JUNG GYUWON <milestogo0905@naver.com>
2023-10-26 01:38:00 +0900

    feat: Question 도메인 객체를 추가
    질문의 내용을 저장하기 위하여 question 객체를 추가하였습니다.

---
+++ src/main/java/happyaging/server/domain/Question.java
@@ -0,0 +1,31 @@
+package happyaging.server.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicUpdate
+@Entity
+@Getter
+@Table(name = "question")
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String question;
+
+    @Column(nullable = false)
+    private String type;
+}

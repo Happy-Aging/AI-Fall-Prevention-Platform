74682aeeb8b586c67e6bc67a7c4fef17dade93ee
JUNG GYUWON <milestogo0905@naver.com>
2024-01-20 10:51:31 +0900

    refactor: WebConfig 클래스 통합

--- src/main/java/happyaging/server/configuration/WebConfig.java
+++ src/main/java/happyaging/server/configuration/WebConfig.java
@@ -1,11 +1,21 @@
 package happyaging.server.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${file.static-image}")
+    private String staticPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/**").addResourceLocations("file:" + staticPath + "/");
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
